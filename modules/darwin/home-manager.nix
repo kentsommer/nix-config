b98344e211749e837730110b87c698869991317d
Kent Sommer <work@kentsommer.com>
2024-07-26 14:52:12 -0700

    Add initial setup
    Quirks:
    * 1password needs manual configuration to setup the ssh agent and
      integrate with the cli  after logging in
    * The 1password-cli seems not to pick up the ssh agent until a solid
      trigger. I was able to do this with `ssh -Tvvv git@github.com`
    * Alfred needs to be configured to include `/nix/store` to find things
      like wezterm, etc.

---
+++ modules/darwin/home-manager.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, lib, home-manager, ... }@inputs:
+
+let name = "Kent Sommer";
+    user = "kentsommer";
+    email = "services@kentsommer.com"; in
+{
+  # Configure user
+  users.users.${user} = {
+    name = "${user}";
+    home = "/Users/${user}";
+    isHidden = false;
+    shell = pkgs.fish;
+  };
+
+  homebrew = {
+    enable = true;
+    casks = pkgs.callPackage ./casks.nix {};
+    masApps = {
+      "line" = 539883307;
+    };
+  };
+
+  # Enable home-manager
+  home-manager = {
+    useGlobalPkgs = true;
+    users.${user} = { pkgs, config, lib, ... }:{
+      home = {
+        enableNixpkgsReleaseCheck = false;
+        packages = pkgs.callPackage ./packages.nix {};
+        file.".wezterm.lua".source = ./dotfiles/wezterm/wezterm.lua;
+        file.".config/lvim/config.lua".source = ./dotfiles/lunarvim/config.lua;
+        file.".ssh/config".source = ./dotfiles/ssh/config;
+        file.".jjconfig.toml".source = ./dotfiles/jj/config.toml;
+        sessionVariables = {
+          EDITOR = "lvim";
+          SSH_AUTH_SOCK = "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+        };
+        stateVersion = "23.11";
+      };
+      imports = [ 
+        inputs.onepassword-shell-plugins.hmModules.default
+      ];
+      programs = {
+        # Shared shell configuration
+        fish = {
+          enable = true;
+          shellAliases = {
+            blaze = "bazelisk";
+            gh = "op plugin run -- gh";
+          };
+        };
+
+        git = {
+          enable = true;
+          ignores = [ "*.swp" ];
+          userName = name;
+          userEmail = email;
+          lfs = {
+            enable = true;
+          };
+          extraConfig = {
+            init.defaultBranch = "main";
+            core = {
+            editor = "lvim";
+              autocrlf = "input";
+            };
+          };
+        };
+
+        neovim = {
+          enable = true;
+        };
+
+        wezterm = {
+          enable = true;
+        };
+
+        zellij = {
+          enable = true;
+        };
+
+        fzf = {
+          enable = true;
+          enableFishIntegration = true;
+        };
+
+        jujutsu = {
+          enable = true;
+        };
+
+        _1password-shell-plugins = {
+          # enable 1Password shell plugins for bash, zsh, and fish shell
+          enable = true;
+          # the specified packages as well as 1Password CLI will be
+          # automatically installed and configured to use shell plugins
+          plugins = with pkgs; [ gh ];
+        };
+
+        gh = {
+          # We disable this so that 1password manages this as a plugin
+          enable = false;
+        };
+      };
+      manual.manpages.enable = true;
+    };
+  };
+}
---
+++ modules/darwin/packages.nix
@@ -0,0 +1,34 @@
+{ pkgs }:
+
+with pkgs; [
+  # General packages for development and system management
+  wezterm
+  coreutils
+  killall
+  openssh
+  wget
+  zip
+  lunarvim
+  bazelisk
+
+  # Encryption and security tools
+  gnupg
+  _1password-gui
+  _1password
+
+  # Media-related packages
+  nerdfonts
+  ffmpeg
+  fzf
+
+  # Text and terminal utilities
+  htop
+  ripgrep
+  zellij
+  unrar
+  unzip
+
+  # Python packages
+  python312
+  python312Packages.virtualenv
+]
